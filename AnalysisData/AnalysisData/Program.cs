2dd58b86ddf631e7b2e55f5952e7a5b938773027
Mahdi Jafari <126339078+mahdijafariii@users.noreply.github.com>
2024-08-13 15:36:02 +0330

    fix(User):fix login (#3)

--- AnalysisData/AnalysisData/Program.cs
+++ AnalysisData/AnalysisData/Program.cs
@@ -1,18 +1,30 @@
+using AnalysisData;
+using AnalysisData.CookieSevice;
+using AnalysisData.CookieSevice.abstractions;
 using AnalysisData.Data;
 using AnalysisData.JwtService;
 using AnalysisData.Repository.RoleRepository;
 using AnalysisData.Repository.RoleRepository.Abstraction;
 using AnalysisData.Repository.UserRepository;
 using AnalysisData.Repository.UserRepository.Abstraction;
+using AnalysisData.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<JwtService>();
+builder.Services.AddScoped<IJwtService,JwtService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ICookieService, CookieService>();
+builder.Services.AddHttpContextAccessor();
+
+
+
+
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();

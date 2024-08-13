29faf57f33cda3b3a038442fcc344d82a894b594
AmirReza <fgg32437@gmail.com>
2024-08-14 02:46:01 +0330

    feat(User):delete migrations

--- AnalysisData/AnalysisData/JwtService/JwtService.cs
+++ AnalysisData/AnalysisData/JwtService/JwtService.cs
@@ -1,6 +1,7 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using AnalysisData.JwtService;
 using AnalysisData.RoleRepository;
 using AnalysisData.UserRepositories.Abstraction;
 using Microsoft.IdentityModel.Tokens;

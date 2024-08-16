c6e78e8e5c1098fec21a5a81bbce146a890e3f58
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-08-16 18:44:04 +0330

    fix(User) : add reset Password api

--- AnalysisData/AnalysisData/Controllers/UserController.cs
+++ AnalysisData/AnalysisData/Controllers/UserController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AnalysisData.Controllers;
+
 [ApiController]
 [Route("api/[controller]")]
 public class UserController : ControllerBase
@@ -15,22 +16,24 @@ public class UserController : ControllerBase
     private readonly IUserService _userService;
     private readonly IPermissionService _permissionService;
 
-    public UserController(IUserService userService,IPermissionService permissionService)
+    public UserController(IUserService userService, IPermissionService permissionService)
     {
         _userService = userService;
         _permissionService = permissionService;
     }
+
     [HttpPost("login")]
     public IActionResult Login([FromBody] UserLoginModel userLoginModel)
     {
         var user = _userService.Login(userLoginModel);
-        return Ok(new { user.Result.FirstName , user.Result.LastName , user.Result.ImageURL });
+        return Ok(new { user.Result.FirstName, user.Result.LastName, user.Result.ImageURL });
     }
+
     [Authorize(Roles = "admin")]
     [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] UserRegisterModel userRegisterModel)
     {
-        var check =  await _userService.Register(userRegisterModel);
+        var check = await _userService.Register(userRegisterModel);
         if (check)
         {
             return Ok("success");
@@ -38,8 +41,8 @@ public class UserController : ControllerBase
 
         return BadRequest("not success");
     }
-    
-    
+
+
     [HttpGet("permissions")]
     public IActionResult GetPermissions()
     {
@@ -48,15 +51,16 @@ public class UserController : ControllerBase
         var username = userClaims.FindFirstValue("username");
         var firstName = userClaims.FindFirstValue("firstname");
         var lastName = userClaims.FindFirstValue("lastname");
-        return Ok(new { username,firstName,lastName,permission });
+        return Ok(new { username, firstName, lastName, permission });
     }
-    
-    
+
+
     [HttpPost("reset-password")]
     public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordModel resetPasswordModel)
     {
         var userClaim = User;
-        var check =  await _userService.ResetPassword(userClaim ,resetPasswordModel.NewPassword);
+        var check = await _userService.ResetPassword(userClaim, resetPasswordModel.NewPassword,
+            resetPasswordModel.ConfirmPassword);
         if (check)
         {
             return Ok("success");

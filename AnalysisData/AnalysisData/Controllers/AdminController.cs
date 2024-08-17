919a869a4edb5c3f141c90f527098f608dee9721
K-Kabiri <126855522+K-Kabiri@users.noreply.github.com>
2024-08-17 22:58:17 +0330

    feat(Admin): Add GetAllUser and GetAllRole methods in AdminController class.

---
+++ AnalysisData/AnalysisData/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using AnalysisData.Services.Abstraction;
+using AnalysisData.UserManage.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnalysisData.Controllers;
+
+[ApiController]
+//[Authorize(Roles = "admin")]
+[Route("api/[controller]")]
+public class AdminController : ControllerBase
+{
+    private readonly IAdminService _adminService;
+
+    public AdminController(IAdminService adminService)
+    {
+        _adminService = adminService;
+    }
+
+    //[Authorize(Roles = "admin")]
+    [HttpGet("GetUsers")]
+    public IActionResult GetAllUsers()
+    {
+        var users = _adminService.GetAllUsers().Result;
+
+        if (users == null || users.Count == 0)
+        {
+            return NoContent();
+        }
+
+        return Ok(users);
+    }
+
+    [HttpGet("GetRoles")]
+    public IActionResult GetAllRoles()
+    {
+        var roles = _adminService.GetAllRoles().Result;
+
+        if (roles == null || roles.Count == 0)
+        {
+            return NoContent();
+        }
+
+        return Ok(roles);
+    }
+    
+    
+}

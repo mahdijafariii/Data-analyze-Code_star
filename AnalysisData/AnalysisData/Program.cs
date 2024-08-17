ff83a234295f9ddadd59080bcb2ffdedde9379da
AmirReza <fgg32437@gmail.com>
2024-08-17 15:27:38 +0330

    feat(Database):Automigration added!

--- AnalysisData/AnalysisData/Program.cs
+++ AnalysisData/AnalysisData/Program.cs
@@ -89,4 +89,17 @@ app.UseMiddleware<JwtMiddleware>();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.EnsureCreated();
+
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+}
 app.Run();

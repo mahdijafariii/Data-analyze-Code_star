a51d9dba0554d441f6c36e1e38ee1856f350963a
AmirReza <fgg32437@gmail.com>
2024-08-19 21:18:11 +0330

    feat(EAV): make cleaner File architecture eav

---
+++ AnalysisData/AnalysisData.migrations/ConsoleStartup.cs
@@ -0,0 +1,36 @@
+﻿using AnalysisData.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public class ConsoleStartup
+{
+    public ConsoleStartup()
+    {
+        var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+        Configuration = builder.Build();
+        
+
+        //.. for test
+        Console.WriteLine(Environment.GetEnvironmentVariable("CONNECTION_STRING"));
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql("Host=localhost;Database=mohaymen;Username=postgres;Password=12345;Timeout=300").UseLazyLoadingProxies();
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+   
+    }
+}

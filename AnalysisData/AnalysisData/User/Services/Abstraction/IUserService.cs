0fb305493bf1b1665605ba814e8490ca71e487a4
K-Kabiri <126855522+K-Kabiri@users.noreply.github.com>
2024-08-27 19:07:46 +0330

    feat(User): Add GetUserInformation method.

--- AnalysisData/AnalysisData/User/Services/Abstraction/IUserService.cs
+++ AnalysisData/AnalysisData/User/Services/Abstraction/IUserService.cs
@@ -9,6 +9,7 @@ namespace AnalysisData.Services.Abstraction;
 public interface IUserService
 {
     Task<User> Login(UserLoginModel userLoginModel);
+    Task<User> GetUser(ClaimsPrincipal userClaim);
     Task<bool> ResetPassword(ClaimsPrincipal userClaim, string password, string confirmPassword);
     Task<bool> UpdateUserInformationByUser(ClaimsPrincipal userClaim, UpdateUserModel updateUserModel);
     Task<bool> NewPassword(ClaimsPrincipal userClaim, string oldPassword, string password, string confirmPassword);
